506b94e435f7b1780beaf837a0b54fb4c3c37e94
James Little <james@ergo.kiwi>
2019-04-02 23:16:11 +1300

    Updated project structure

---
+++ tests/README.md
@@ -0,0 +1,9 @@
+# Python unittest
+
+These can be run directly in PyCharm.
+
+Run as script. For example
+
+```bash
+ python -m unittest tests/test-kata02-binary-search.py in code-kata-python
+```
---
+++ tests/kata02/test_binary_search.py
@@ -0,0 +1,39 @@
+from tests import kata02
+from tests.kata02.context import chop
+
+import unittest
+
+
+class Kata02TestSuite(unittest.TestCase):
+    """Basic test cases for Kata 02 - the karate chop."""
+
+    def test_empty_array(self):
+        self.assertEqual(-1, chop(3, []))
+
+    def test_single(self):
+        self.assertEqual(-1, chop(3, [1]))
+        self.assertEqual(0, chop(1, [1]))
+
+    def test_3_items(self):
+        self.assertEqual(0, chop(1, [1, 3, 5]))
+        self.assertEqual(1, chop(3, [1, 3, 5]))
+        self.assertEqual(2, chop(5, [1, 3, 5]))
+        self.assertEqual(-1, chop(0, [1, 3, 5]))
+        self.assertEqual(-1, chop(2, [1, 3, 5]))
+        self.assertEqual(-1, chop(4, [1, 3, 5]))
+        self.assertEqual(-1, chop(6, [1, 3, 5]))
+
+    def test_4_items(self):
+        self.assertEqual(0, chop(1, [1, 3, 5, 7]))
+        self.assertEqual(1, chop(3, [1, 3, 5, 7]))
+        self.assertEqual(2, chop(5, [1, 3, 5, 7]))
+        self.assertEqual(3, chop(7, [1, 3, 5, 7]))
+        self.assertEqual(-1, chop(0, [1, 3, 5, 7]))
+        self.assertEqual(-1, chop(2, [1, 3, 5, 7]))
+        self.assertEqual(-1, chop(4, [1, 3, 5, 7]))
+        self.assertEqual(-1, chop(6, [1, 3, 5, 7]))
+        self.assertEqual(-1, chop(8, [1, 3, 5, 7]))
+
+
+if __name__ == '__main__':
+    unittest.main()
