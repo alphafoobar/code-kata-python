51158c88bedb2712892c32bcc957520f7407c285
James Little <james@ergo.kiwi>
2019-04-02 23:21:18 +1300

    Updated test structure for kata04

--- tests/kata04/test_weather_data.py
+++ tests/kata04/test_weather_data.py
@@ -1,4 +1,4 @@
-from .context import kata04
+from tests.kata04.context import smallest_spread
 
 import unittest
 
@@ -7,7 +7,7 @@ class WeatherDataTestSuite(unittest.TestCase):
     """Basic test cases for Kata 04 - Data Munging."""
 
     def test_no_data(self):
-        self.assertEqual(0, kata04.smallest_difference([]))
+        self.assertEqual(0, smallest_spread([]))
 
 
 if __name__ == '__main__':
